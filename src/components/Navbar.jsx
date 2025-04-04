f4eaf961758be9825f641123285bbeb716c6194a
Sumit Joshi <sumit.joshi0616@gmail.com>
2025-04-04 20:19:12 +0530

    Added project section

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,25 +1,45 @@
-import logo from "../assets/kevinRushLogo.png"
-import {FaLinkedin} from "react-icons/fa"
-import {FaGithub} from "react-icons/fa"
-import {FaInstagram} from "react-icons/fa"
-import {FaTwitter} from "react-icons/fa"
-import React from 'react'
+import logo from "../assets/kevinRushLogo.png";
+import { FaLinkedin, FaGithub, FaInstagram, FaTwitter } from "react-icons/fa";
+import React from "react";
 
 function Navbar() {
   return (
-   <nav className=' mb-20 flex items-center justify-between py-6'>
-    <div className='flex flex-shrink-0 items-center'>
-       <img className='mx-2 w-10' src={logo} alt="logo"/>
-    </div>
-    <div className="m-8 flex items-center justify-center gap-4 text-2xl">
-        <FaLinkedin/>
-        <FaGithub/>
-        <FaInstagram/>
-        <FaTwitter/>
-
-    </div>
-   </nav>
-  )
+    <nav className="mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img className="mx-2 w-10" src={logo} alt="logo" />
+      </div>
+      <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+        <a
+          href="https://www.linkedin.com/in/sumit-joshi-417a91258/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaLinkedin className="hover:text-blue-600" />
+        </a>
+        <a
+          href="https://github.com/josum06"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="hover:text-gray-400" />
+        </a>
+        <a
+          href="https://www.instagram.com/your-profile"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaInstagram className="hover:text-pink-500" />
+        </a>
+        <a
+          href="https://x.com/SumitJo26139837"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaTwitter className="hover:text-blue-400" />
+        </a>
+      </div>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,30 +1,115 @@
 import React from 'react'
-import { FaReact, FaNodeJs } from "react-icons/fa";  // React & Node.js icons
-import { SiMongodb, SiNextdotjs , SiExpress} from "react-icons/si"; // MongoDB & Next.js icons
+import { FaReact, FaNodeJs, FaHtml5, FaCss3Alt, FaJs } from "react-icons/fa";  // React & Node.js icons
+import { SiMongodb, SiNextdotjs , SiExpress, SiCplusplus} from "react-icons/si"; // MongoDB & Next.js icons
+import { animate, motion } from 'framer-motion';
 
 
+const iconVariants = (duration) => ({
+  initial : {y: -10},
+  animate:{
+     y: [10,-10],
+     transition : {
+       duration: duration,
+       ease: "linear",
+       repeat: Infinity,
+       repeatType: "reverse"
+     },
+  },
+})
 function Technologies() {
   return (
     <div className='border-b border-neutral-800 pb-24'>
-      <h2 className='my-20 text-center text-4xl'>Technogies</h2>
-      <div className='flex flex-wrap items-center justify-center gap-4'>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+      <motion.h1 
+        whileInView={{opacity:1 ,y:0}}
+        initial={{opacity:0 ,y: -100}}
+        transition={{duration: 1.5}}
+        
+        className='my-20 text-center text-4xl'>Technogies</motion.h1>
+      <motion.div 
+         whileInView={{opacity: 1,x:0}}
+         initial={{opacity:0, x:-100}}
+         transition={{duration: 1.5}}
+         className='flex flex-wrap items-center justify-center gap-4'>
+        <motion.div 
+          variants={iconVariants(2.5)}
+          initial="initial"
+          animate="animate"
+          className='rounded-2xl border-4 border-neutral-800 p-4'>
           <FaReact  className='text-7xl text-cyan-400 '/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-          <FaNodeJs  className='text-7xl text-green-500 '/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-          <SiMongodb  className='text-7xl text-green-500 '/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-          <SiNextdotjs  className='text-7xl  '/>
-        </div>
-        <div className='rounded-2xl border-4 border-neutral-800 p-4'>
-          <SiExpress  className='text-7xl  text-yellow-500'/>
-        </div>
+        </motion.div>
 
-      </div>
+        <motion.div 
+          variants={iconVariants(3)}
+          initial="initial"
+          animate="animate"
+          className='rounded-2xl border-4 border-neutral-800 p-4'>
+          <FaNodeJs  className='text-7xl text-green-500 '/>
+        </motion.div>
+
+
+        <motion.div 
+          variants={iconVariants(5)}
+          initial="initial"
+          animate="animate"
+          className='rounded-2xl border-4 border-neutral-800 p-4'>
+          <SiMongodb  className='text-7xl text-green-500 '/>
+        </motion.div>
+
+        <motion.div 
+          variants={iconVariants(2)}
+          initial="initial"
+          animate="animate"
+          className='rounded-2xl border-4 border-neutral-800 p-4'>
+          <SiNextdotjs  className='text-7xl  '/>
+        </motion.div>
+
+
+        <motion.div 
+          variants={iconVariants(4)}
+          initial="initial"
+          animate="animate"
+          className='rounded-2xl border-4 border-neutral-800 p-4'>
+          <SiExpress  className='text-7xl  text-yellow-500'/>
+        </motion.div>
+
+        <motion.div
+           variants={iconVariants(2)}
+           initial="initial"
+           animate="animate"
+           className='rounded-2xl border-4 border-neutral-800 p-4'
+         >
+          <FaHtml5 className='text-7xl text-orange-500'/>
+        </motion.div>
+
+        <motion.div
+           variants={iconVariants(3)}
+           initial="initial"
+           animate="animate"
+           className='rounded-2xl border-4 border-neutral-800 p-4'
+         >
+          <FaCss3Alt className='text-7xl text-blue-500'/>
+        </motion.div>
+
+        <motion.div
+           variants={iconVariants(2.5)}
+           initial="initial"
+           animate="animate"
+           className='rounded-2xl border-4 border-neutral-800 p-4'
+         >
+          <FaJs className='text-7xl text-yellow-300'/>
+        </motion.div>
+        <motion.div
+           variants={iconVariants(2.5)}
+           initial="initial"
+           animate="animate"
+           className='rounded-2xl border-4 border-neutral-800 p-4'
+         >
+          <SiCplusplus className='text-7xl text-blue-300'/>
+        </motion.div>
+
+
+          
+      </motion.div>
     </div>
   )
 }
